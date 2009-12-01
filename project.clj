b088861de838d1dbff91fe062442f37053df8b6b
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2009-12-01 20:36:04 +0100

    Add a preliminary Leiningen project definition.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject logikorr "0.0.1-SNAPSHOT"
+  :description "Ein einfaches Hausaufgabenkorrekturpunkteeingabesystem."
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                 [org.clojars.ato/compojure "0.3.1"]
+                 [org.danlarkin/clojure-json "1.1-SNAPSHOT"]
+                 [appengine-clj "0.0.0"]  ;FIXME
+                 ]
+  :dev-dependencies [[org.clojure/swank-clojure "1.0"]])
